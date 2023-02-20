61a569cc0c98354a560e5ce7e18d24b4d68b8b1d
melihkaragoz <melihkkaragoz@hotmail.com>
2023-02-20 06:37:34 +0300

    not yet

--- collage.py
+++ collage.py
@@ -12,11 +12,16 @@ d.pop(-1)
 print(d)
 new_image = Image.new('RGB',(pc_width, pc_height))
 width = 0
+height = 400
 for url in d:
 	image1 = Image.open(f"{url}")
-	image1 = image1.resize(size=[int(pc_width/len(d)), 400])
-	new_image.paste(image1,(width,0))
+	if (width >= pc_width-100):
+			width = 0
+			height += 400
+
+	image1 = image1.resize(size=[int(pc_width/len(d)), height])
+	new_image.paste(image1,(width,int(pc_height/2)-200))
 	width += int(pc_width/len(d))
 
 new_image.save("kolaj.jpg","JPEG")
-#os.system("bash ./changeBackground.sh")
+os.system("bash ./changeBackground.sh")
