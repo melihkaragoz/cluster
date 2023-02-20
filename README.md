78efeecd47cb66e5fae956b3ef8e9b27d006d3f1
melihkaragoz <melihkkaragoz@hotmail.com>
2023-02-20 20:08:35 +0300

    token field seperated

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-NOTE
+#NOTE
 ----------------------
-1 - access token's expire date is 2hr later from when it's created.
-2 - don't forget to change ".jpg" file's path in changeBackground.sh.
-----------------------
+1 - access token's expire date is 2hr later from when it's created.<br>
+2 - don't forget to change ".jpg" file's path in changeBackground.sh.<br>
+3 - run './generateAccessToken' after setting your user_id and secret_token field in 'generateAccessToken.sh', for generate new access token.<br>
 
-installing
+#installing
 ----------------------
-pip3 install wget
-pip3 install Pillow
-----------------------
+pip3 install wget<br>
+pip3 install Pillow<br>
--- cluster.py
+++ cluster.py
@@ -1,7 +1,9 @@
 import json,os,wget
 import ssl
+from time import sleep
 
-accessToken = "90b9270743f040399efb572dbd71522df41622ab517d59cb20906bcbaf65ad6d"
+t = open("token.txt","r")
+accessToken = t.readline()
 ssl._create_default_https_context = ssl._create_unverified_context
 users = []
 available = []
@@ -17,10 +19,15 @@ os.mkdir("photos")
 os.chdir("./photos")
 try:
 	for user in users:
+		sleep(0.3)
 		os.system(f"curl  -H 'Authorization: Bearer {accessToken}' 'https://api.intra.42.fr/v2/users/{user}/' > res.json")
 		f = open("res.json","r")
 		data = json.load(f)
-		d = data["location"]
+		try:
+			d = data["location"]
+		except:
+			print("Token expired, please run the 'generateAccessToken.sh' script with your user_id and secret_key.")
+			exit()
 		img = data["image"]["link"]
 		dict = {
 			"user" : user,
@@ -30,7 +37,7 @@ try:
 		if (d != None):
 			available.append(dict)
 except:
-	print(" -- Token expired !! -- ")
+	print(" -- An error occured !! for help : mkaragoz at 42/Istanbul -- ")
 
 for u in available:
 	wget.download(f"{u['img']}")
