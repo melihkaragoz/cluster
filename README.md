b8387117dccd8bbd2eb4a7b68fb12e4a8f12113a
melihkaragoz <melihkkaragoz@hotmail.com>
2023-02-20 21:36:08 +0300

    updated README.md and collage.py

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 1 - access token's expire date is 2hr later from when it's created.<br>
 2 - don't forget to change ".jpg" file's path in changeBackground.sh.<br>
 3 - run './generateAccessToken' after setting your user_id and secret_token field in 'generateAccessToken.sh', for generate new access token.<br>
+4 - specify your pc's screen size values (width,height) in 'collage.py' script.<br>
 
 #installing
 ----------------------
--- collage.py
+++ collage.py
@@ -1,8 +1,8 @@
 import os
 from PIL import Image
 
-pc_width = 5120
-pc_height = 2880
+pc_width = 2560
+pc_height = 1600
 os.system("ls photos/*.jpg > ./photos/urls")
 f = open("./photos/urls","r")
 d = f.read()
@@ -11,16 +11,16 @@ d.pop(-1)
 print(d)
 new_image = Image.new('RGB',(pc_width, pc_height))
 width = 0
-height = 400
+height = int(pc_width/6)
 for url in d:
 	image1 = Image.open(f"{url}")
-	image1 = image1.resize(size=[400, 400])
-	new_image.paste(image1,(width,int(pc_height/2)-200))
-	if (width + 400 >= pc_width):
+	image1 = image1.resize(size=[int(pc_width/6), int(pc_width/6)])
+	new_image.paste(image1,(width,(int(pc_height/2)-int(pc_width/12))))
+	if (width + 800 >= pc_width):
 		width = 0
-		height += 400
+		height += int(pc_width/6)
 	else:
-		width += 400
+		width += int(pc_width/6)
 
 new_image.save("collage.jpg","JPEG")
 os.system("bash ./changeBackground.sh")
