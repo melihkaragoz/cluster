c6d02f774b226e1c84ee991546fa735f9b576b05
melihkaragoz <melihkkaragoz@hotmail.com>
2023-02-20 06:08:40 +0300

    some fixes

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-create "photos" folder
+NOTE
+---------------------
+don't forget to create "photos" folder
+---------------------
+
+installing
 ----------------------
 pip3 install wget
 pip3 install Pillow
 ----------------------
 
-get accessToken
-
+getting access token
+---------------------
+Format : 
 curl -X POST --data "grant_type=client_credentials&client_id=<YOUR_CLIENT_ID>&client_secret=<YOUR_SECRET>" https://api.intra.42.fr/oauth/token
+
+Example : 
+curl -X POST --data "grant_type=client_credentials&client_id=u-s4t2ud-e6a9641743be40bf815edf146640a77e92c8d38dee9780bb49de2a74df3afa4e&client_secret=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" https://api.intra.42.fr/oauth/token
+---------------------
--- cluster.py
+++ cluster.py
@@ -1,24 +1,27 @@
 import json,os,wget
 import ssl
 
-accessToken = "32fc7cf76c4104584a85f0230e6db08d906ce659ec232d86a490eda003229782"
+accessToken = "90b9270743f040399efb572dbd71522df41622ab517d59cb20906bcbaf65ad6d"
 ssl._create_default_https_context = ssl._create_unverified_context
 users = ["mkaragoz","makbulut","mdiraga"]
 available = []
 os.chdir("./photos")
-for user in users:
-	os.system(f"curl  -H 'Authorization: Bearer {accessToken}' 'https://api.intra.42.fr/v2/users/{user}/' > res.json")
-	f = open("res.json","r")
-	data = json.load(f)
-	d = data["location"]
-	img = data["image"]["link"]
-	dict = {
-		"user" : user,
-		"location" : d,
-		"img" : img
-	}
-	if (d != None):
-		available.append(dict)
+try:
+	for user in users:
+		os.system(f"curl  -H 'Authorization: Bearer {accessToken}' 'https://api.intra.42.fr/v2/users/{user}/' > res.json")
+		f = open("res.json","r")
+		data = json.load(f)
+		d = data["location"]
+		img = data["image"]["link"]
+		dict = {
+			"user" : user,
+			"location" : d,
+			"img" : img
+		}
+		if (d != None):
+			available.append(dict)
+except:
+	print(" -- Token expired !! -- ")
 
 for u in available:
 	wget.download(f"{u['img']}")
